f7564872a7a879889e6f3b5686c66bf7eb883660
Mostafa Khazaei Panah <68190739+Mkpanah@users.noreply.github.com>
2024-09-26 18:48:03 +0330

    Update 3 - Decimal to Binary via Recursion.py

--- 3 - Decimal to Binary via Recursion.py
+++ 3 - Decimal to Binary via Recursion.py
@@ -2,7 +2,8 @@ def convert(num):
     if num == 0:
         return 0
     else:
-        return (num % 2 + 10 * convert(num//2))
+        return num % 2 + 10 * convert(num//2)
+
 
 number = int(input("Enter a Decimal Integer Number: "))
 print(f"The Binary format of {number} is {convert(num=number)}.")
